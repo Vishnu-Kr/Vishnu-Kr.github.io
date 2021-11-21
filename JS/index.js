cbefcb821ece026f158cbc8efff1856da2dd96ec
Vishnu-Kr <vishnu.kumardgp2002@gmail.com>
2021-11-21 15:53:12 +0530

    files upload

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+document.write("Im Vishnu Kumar");
+var num1=6;
+console.log(num1);
+console.log(typeof num1);
+num1= [5,6,7,8]
+console.log(num1.length);
+
+num1=prompt("enter rollno");
+console.log(typeof num1);
